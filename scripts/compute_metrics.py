87ea8a2848eb2f62d77011221b38e9670f046d56
Timothy Tran <timothtran@gmail.com>
2024-12-11 13:26:05 +0100

    Refractored code to move all data preprocessing into scripts, also moved metrics computation to scripts

---
+++ scripts/compute_metrics.py
@@ -0,0 +1,164 @@
+# Notebook for computing metrics
+
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.metrics import (average_precision_score,
+                             PrecisionRecallDisplay,
+                             precision_recall_curve,
+                             matthews_corrcoef,
+                             roc_auc_score)
+from scipy.special import softmax
+
+
+def compute_metrics(eval_pred, filename):
+    """
+    Function to simultaneously evaluate accuracy, F1 and average precision (AP)
+
+    The function does the evaluation per label for accuracy, and per token otherwise.
+
+    average precision is the most interesting as it accounts for the fact that the
+    ideal decision boundary may be something non trivial.
+
+    F1 and accuracy are reported at the decision boundary which maximises the F1
+    """
+    raw_predictions, labels = eval_pred
+
+    logits = raw_predictions # Discard hidden states and keep logits
+
+    # Find predictions from logits
+    predictions = softmax(logits, axis=2)[:,:,1] # Probability of positive label
+
+    # Reshape predictions and labels into long strings to compute metrics per token
+    predictions_flat = predictions.reshape((-1,))
+    labels_flat = labels.reshape((-1,))
+
+    # Remove all the padded ones
+    predictions_flat = predictions_flat[labels_flat!=-100]
+    labels_flat_cleaned = labels_flat[labels_flat!=-100]
+
+    # Compute average precision
+    AP = average_precision_score(labels_flat_cleaned, predictions_flat)
+
+    # Plot precision curves
+    precision, recall, thresholds = precision_recall_curve(labels_flat_cleaned, predictions_flat)
+    fig, ax = plt.subplots(dpi = 300, figsize = (5,3))
+    ax.set_ylabel("Precision")
+    ax.set_xlabel("Recall")
+    ax.set_title("Precision-Recall Curve at final epoch: splicebert", fontsize = 12)
+    ax.plot(recall, precision)
+    fig.savefig("Precision-Recall Curve at final epoch: splicebert.png")
+
+    np.savetxt(f'pr_curve_{filename}.txt', np.vstack((precision,recall)).T)
+
+    #Compute ideal boundary and optimized F1
+    ideal_threshold = thresholds[np.nanargmax(2 * (precision * recall) / (precision + recall))]
+    F1 = np.nanmax(2 * (precision * recall) / (precision + recall))
+
+    #Calculate accuracy
+    categorical_predictions = np.where(predictions>ideal_threshold, 1, 0)
+    accuracy = compute_accuracy(labels, categorical_predictions)
+
+    categorical_predictions_flat = categorical_predictions.reshape((-1,))
+    categorical_predictions_flat = categorical_predictions_flat[labels_flat!=-100]
+
+    MCC = matthews_corrcoef(labels_flat_cleaned, categorical_predictions_flat)
+    AUC = roc_auc_score(labels_flat_cleaned, categorical_predictions_flat)
+
+    #Combine metrics
+    dictionary = {"F1" : F1} | {"Seq. acc." : accuracy} | {"AP" : AP} | {"MCC" : MCC} | {"AUC" : AUC} | {"ideal_threshold" : ideal_threshold}
+
+    #Save the performance metrics to a text file
+    with open(f'performance_metrics_{filename}.txt', 'w') as f:
+      print(dictionary, file=f)
+
+    #Return joint dictionary
+    return dictionary
+
+
+def compute_accuracy(labels, categorical_predictions):
+    sequence_matches = 0
+    total_sequences = 0
+
+    for label, cat_preds in zip(labels, categorical_predictions):
+        # Ignore padded tokens
+        cat_preds = cat_preds[label != -100]
+        label = label[label != -100]
+
+        assert len(cat_preds) == len(label)
+
+        # Sequence-level accuracy
+        if np.array_equal(label, cat_preds):  # Entire sequence matches
+            sequence_matches += 1
+        total_sequences += 1
+
+    # Sequence-level accuracy
+    sequence_accuracy = sequence_matches / total_sequences if total_sequences > 0 else 0
+    return sequence_accuracy
+
+
+# Hidden states of all test samples is order 260GB
+# Get rid of these during evaluation to not run out of RAM
+def preprocess_logits_for_metrics(logits, labels):
+  """
+  The metric function needs the logits of all samples in the test set loaded in RAM
+  simultaneously to evaluate performance. Model output contains both logits and
+  Hidden states, hidden states take up much more space.
+
+  We inject this function to the training loop to delete hidden states while
+  evaluating the test samples. This frees tons of memory.
+  """
+  logits = logits[0] # Discard hidden states
+  return logits
+
+
+def compute_metrics_test(eval_pred, file_name, decision_threshold):
+    """
+    Same as above "compute_metrics" function, but without decision boundary optimization.
+    """
+    logits, labels = eval_pred
+
+    #Find predictions from logits
+    predictions = softmax(logits, axis=2)[:,:,1] #probability of positive label
+
+    #Reshape predictions and labels into long strings to compute metrics per token
+    predictions_flat = predictions.reshape((-1,))
+    labels_flat = labels.reshape((-1,))
+
+    #Remove all the padded ones
+    predictions_flat = predictions_flat[labels_flat!=-100]
+    labels_flat_cleaned = labels_flat[labels_flat!=-100]
+
+    #compute average precision
+    AP = average_precision_score(labels_flat_cleaned, predictions_flat)
+
+    #Plot precision curves
+    precision, recall, thresholds = precision_recall_curve(labels_flat_cleaned, predictions_flat)
+    fig, ax = plt.subplots(dpi = 300, figsize = (5,3))
+    ax.set_ylabel("Precision")
+    ax.set_xlabel("Recall")
+    ax.set_title("Precision-Recall Curve at final epoch: splicebert", fontsize = 12)
+    ax.plot(recall, precision)
+    fig.savefig(file_name + ".png")
+
+    np.savetxt("pr_curve.txt", np.vstack((precision,recall)).T)
+
+    #Calculate accuracy
+    categorical_predictions = np.where(predictions>decision_threshold, 1, 0)
+    accuracy = compute_accuracy(labels, categorical_predictions)
+
+    categorical_predictions_flat = categorical_predictions.reshape((-1,))
+    categorical_predictions_flat = categorical_predictions_flat[labels_flat!=-100]
+
+    F1 = F1_calc.compute(predictions=categorical_predictions_flat, references=labels_flat_cleaned)
+    MCC = matthews_corrcoef(labels_flat_cleaned, categorical_predictions_flat)
+    AUC = roc_auc_score(labels_flat_cleaned, categorical_predictions_flat)
+
+    #Combine metrics
+    dictionary = F1 | {"Seq. Acc" : accuracy} | {"AP" : AP} | {"MCC" : MCC} | {"AUC" : AUC}
+
+    #Save the performance metrics to a text file
+    with open(file_name + ".txt", 'w') as f:
+      print(dictionary, file=f)
+
+    #Return joint dictionary
+    return dictionary
---
+++ scripts/trainer_datasets_creation.py
@@ -0,0 +1,37 @@
+from datasets import Dataset
+from .data_preprocessing import *
+
+def create_dataset(df, tokenizer, model, max_model_input_size, seed, sample_n_datapoints=None, truncate=True, shuffle=True):
+    if sample_n_datapoints:
+        df = df.sample(n = sample_n_datapoints, random_state=seed)
+    
+    # Calculate BP_POS_WITHIN_STRAND
+    df['BP_POS_WITHIN_STRAND'] = df['IVS_SIZE'] + df['BP_ACC_DIST']
+
+    # Remove all data points where the BP is farther than
+    df = df[df['IVS_SIZE'] - df['BP_POS_WITHIN_STRAND'] <= max_model_input_size]
+
+    # Split to train/val/test
+    train_df, test_df, val_df = split_train_test_on_chr(df, shuffle, seed)
+
+    # Extract the sequences and labels that the model will train on
+    train_seqs, train_labels = extract_intron_seq_and_labels(train_df, max_model_input_size, truncate=True)
+    test_seqs, test_labels = extract_intron_seq_and_labels(test_df, max_model_input_size, truncate=True)
+    val_seqs, val_labels = extract_intron_seq_and_labels(val_df, max_model_input_size, truncate=True)
+
+    # Tokenize the sequences
+    train_ids = tokenizer(train_seqs, padding_side = 'left', padding = 'max_length')
+    test_ids = tokenizer(test_seqs, padding_side = 'left', padding = 'max_length')
+    val_ids = tokenizer(val_seqs, padding_side = 'left', padding = 'max_length')
+
+    # Build the dataset structure that will be passed for training
+    train_dataset = Dataset.from_dict(train_ids)
+    train_dataset = train_dataset.add_column("labels", train_labels)
+
+    val_dataset = Dataset.from_dict(val_ids)
+    val_dataset = val_dataset.add_column("labels", val_labels)
+
+    test_dataset = Dataset.from_dict(test_ids)
+    test_dataset = test_dataset.add_column("labels", test_labels)
+
+    return train_dataset, val_dataset, test_dataset
